7c70834ea99ff9d293db7255355a5a8248569b04
Aline Tavares <aline@crowdbotics.com>
2023-05-17 13:32:36 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "breakingapp-0617-de-84104",
-  url: "https://breakingapp-0617-de-84104.botics.co",
-  api: "https://breakingapp-0617-de-84104.botics.co/api/v1"
-}
+  "name": "breakingapp-0617-de-84104",
+  "url": "https://breakingapp-0617-de-84104.botics.co",
+  "api": "https://breakingapp-0617-de-84104.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,30 @@
+import { Text } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled1 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><Text style={styles.mDmjPYyU}>{"Basic Studio Screen"}</Text></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  mDmjPYyU: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    top: 261,
+    left: 128
+  }
+});
+export default Untitled1;
